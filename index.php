927ffb793ea3ffb07c6a77df0b7a6113c01f9cb1
moondev0 <elmasinc02@gmail.com>
2022-12-27 23:58:21 +0100

    second commit

--- index.php
+++ index.php
@@ -5,17 +5,17 @@ $serviceCode = $_POST["serviceCode"];
 $text = $_POST["text"];
 //This is the first menu screen
 if ( $text == "" ) {
-$response  = "CON Hi welcome, I can help you with Event Reservation \n";
+$response  = "CON Emergency Hub, Choose a portal \n";
 $response .= "1. Enter 1 to continue";
 }
 // Menu for a user who selects '1' from the first menu
 // Will be brought to this second menu screen
 else if ($text == "1") {
 $response  = "CON  Pick a table for reservation below \n";
-$response .= "1. Table for 2 \n";
-$response .= "2. Table for 4 \n";
-$response .= "3. Table for 6 \n";
-$response .= "4. Table for 8 \n";
+$response .= "1. Federal Road safety \n";
+$response .= "2. Nierian Police \n";
+$response .= "3. Fire fighters \n";
+$response .= "4. More \n";
 }
 //Menu for a user who selects '1' from the second menu above
 // Will be brought to this third menu screen
